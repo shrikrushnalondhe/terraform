f650ac23d6441286097abe313c3dd01d5396ddf7
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-09-14 10:55:40 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "ec2-sgnew" {
    protocol    = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+   from_port   = 8080
+   to_port     = 8080
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
    from_port   = 0
