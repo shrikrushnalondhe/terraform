df589191273c1ed0ab0406230451a6583a96ab3e
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-09-14 10:52:31 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -41,12 +41,12 @@ resource "aws_instance" "tomcat" {
       type        = "ssh"
       host        = self.public_ip
       user        = "jenkins"
-      private_key = file("/home/jenkins/keys/aws_key")
+      private_key = file("/home/jenkins/keys1/aws_key")
       timeout     = "4m"
    }
 }
 resource "aws_key_pair" "deployer" {
-  key_name   = "aws_key"
+  key_name   = "aws_key1"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9pKGwpIYn4jiBZGQfs9T7lGUHOvjurO5nU02y4jXEExQ7G+c3OwmNQN/d4F8OtTYtKl5PcIRWEvHRPQ3cUT3KooFKCZIpoSmxLLhFTFv96wi1KFjMMs25D6YR1mBj2XVXdDJe0DvFL/2xcHf+4U2zUrm6m6AMyfmu2U58Ls/67uLG3Y+2mGcuBBv//oC70oQM3thYlW74xw5nNUC91LKZnuUg6ZOMGPc3AeLnZhJz0SBBJxhe04xy5iWHk+CPNgU5Dgt6kFLmWV+XrBAP5k+AfXb7ipgpwYiFi/Oq8f46h95TVHTJZydAjswoz3GdS2lcd7R26AtSybnECn5/OOBf jenkins@ip-172-31-23-199.ec2.internal"
 }
 output "instance_ips" {
