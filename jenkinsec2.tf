d9d2333d4ad8cda9c745f77b04d13a354aaa25fe
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-01 14:27:43 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "ap-south-1"
  }
 resource "aws_security_group" "ec2-sgnew" {
   name = "ec2-sgnew"
@@ -36,19 +36,19 @@ resource "aws_security_group" "ec2-sgnew" {
   }
 }
 
-resource "aws_instance" "tomcat" {
-  ami = "ami-087c17d1fe0178315"
+resource "aws_instance" "dockerec2" {
+  ami = "ami-0a23ccb2cdd9286bb"
   instance_type = "t2.micro"
   key_name= "aws_key"
   security_groups = [aws_security_group.ec2-sgnew.name]
   tags = {
-    Name = "tomcat"
+    Name = "dockerec2"
   }
   connection {
       type        = "ssh"
       host        = self.public_ip
       user        = "jenkins"
-      private_key = file("/home/jenkins/keys1/aws_key")
+      private_key = file("/home/jenkins/keys/aws_key")
       timeout     = "4m"
    }
 }
@@ -57,5 +57,5 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9pKGwpIYn4jiBZGQfs9T7lGUHOvjurO5nU02y4jXEExQ7G+c3OwmNQN/d4F8OtTYtKl5PcIRWEvHRPQ3cUT3KooFKCZIpoSmxLLhFTFv96wi1KFjMMs25D6YR1mBj2XVXdDJe0DvFL/2xcHf+4U2zUrm6m6AMyfmu2U58Ls/67uLG3Y+2mGcuBBv//oC70oQM3thYlW74xw5nNUC91LKZnuUg6ZOMGPc3AeLnZhJz0SBBJxhe04xy5iWHk+CPNgU5Dgt6kFLmWV+XrBAP5k+AfXb7ipgpwYiFi/Oq8f46h95TVHTJZydAjswoz3GdS2lcd7R26AtSybnECn5/OOBf jenkins@ip-172-31-23-199.ec2.internal"
 }
 output "instance_ips" {
-  value = aws_instance.tomcat.public_ip
+  value = aws_instance.dockerec2.public_ip
 }
